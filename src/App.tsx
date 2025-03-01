051081ecb5a0256141f2a761df2ae0e4cc688349
Saikat Paul <saptadipa123@gmail.com>
2025-03-02 01:03:11 +0530

    Adds more functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import { uniqueId } from "lodash";
 import "./App.css";
 
 interface IPlaylistItem {
   name: string;
   url: string;
+  id: string;
 }
 
 function App() {
@@ -15,41 +17,62 @@ function App() {
     const file = e.target.files[0];
     if (file) {
       const url = URL.createObjectURL(file);
-      setPlaylist((prev) => [...prev, { name: file.name, url }]);
+      setPlaylist((prev) => [
+        ...prev,
+        { name: file.name, url, id: uniqueId() },
+      ]);
     }
+  };
+
+  const clearAll = () => {
+    setPlaylist([]);
+    setSelectedItem(null);
   };
 
   return (
     <div className="wrapper">
       <ul className="playlist">
-        {playlist.map((item, idx) => (
-          <li className="playlist-item" key={idx} onClick={() => setSelectedItem(item)}>
+        {playlist.map((item) => (
+          <li
+            className={`playlist-item ${
+              item.id === selectedItem?.id ? "playlist-item-selected" : ""
+            }`}
+            key={item.id}
+            onClick={() => setSelectedItem(item)}
+          >
             {item.name}
           </li>
         ))}
         <li className="playlist-item playlist-item-upload">
           <p>Upload here</p>
           <input
-          className="file-input"
+            className="file-input"
             type="file"
             style={{ fontSize: "50px" }}
             onChange={handleUpload}
           />
         </li>
       </ul>
-      <div className="player">
-        {selectedItem && (
-          <video
-            controls
-            src={selectedItem.url}
-            onEnded={() => {
-              const idx = playlist.findIndex((item) => item.url === selectedItem.url);
-              if (idx < playlist.length - 1) {
-                setSelectedItem(playlist[idx + 1]);
-              }
-            }}
-          />
-        )}
+      <div className="player-wrapper">
+        <div className="player">
+          {selectedItem && (
+            <video
+              controls
+              src={selectedItem.url}
+              onEnded={() => {
+                const idx = playlist.findIndex(
+                  (item) => item.url === selectedItem.url
+                );
+                if (idx < playlist.length - 1) {
+                  setSelectedItem(playlist[idx + 1]);
+                }
+              }}
+            />
+          )}
+        </div>
+        <div>
+          <button onClick={clearAll}>Clear all</button>
+        </div>
       </div>
     </div>
   );
